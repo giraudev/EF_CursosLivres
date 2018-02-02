15a67f31cae23d6393c79ff15ce301ded3ced206
Giraudev <giraudev@gmail.com>
2018-02-02 21:43:40 -0200

    add swagger

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using CursosLivre.Dados;
@@ -9,6 +10,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace CursosLivre
 {
@@ -26,6 +28,29 @@ namespace CursosLivre
         {
             services.AddDbContext<CursosLivreContexto>(opt => opt.UseSqlServer(Configuration.GetConnectionString("BancoCursosLivre")));
             services.AddMvc();
+
+            //adicionando o Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    //dados do arquivo
+                    Version = "v1",
+                    Title = "Cursos Livre",
+                    Description = "Teste Simples",
+                    TermsOfService = "None",
+                    Contact = new Contact { Name = "Gabriela Giraud", Email = "giraudev@gmail.com", Url = "http:///github.com/giraudev" }
+                });
+
+                //caminho onde está a documentação
+                var basePath = AppContext.BaseDirectory;
+                //pegar os comentários no arquivo .xml
+                var xmlPath = Path.Combine(basePath, "CursosLivre.xml");
+
+                //comando para incluir os comentários dentro do arquivo
+                c.IncludeXmlComments(xmlPath);
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +62,17 @@ namespace CursosLivre
             }
 
             app.UseMvc();
+
+            //código para utilizar o Swagger
+            app.UseSwagger();
+
+            //UI: user interface
+            app.UseSwaggerUI(c =>
+            {
+                //para o usuário acessar a documentação, definir a url
+                //"My API V1" é a descrição
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.Run(async (context) =>
             {
